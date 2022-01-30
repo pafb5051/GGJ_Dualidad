0f0ace094c28a5128bbf997934c2282159e71c5c
JARiveraB <68044277+JARiveraB@users.noreply.github.com>
2022-01-30 16:56:56 -0600

    SubMainMenu

--- GGJ_Dualidad_UnityProject/Assets/mainMenu.cs
+++ GGJ_Dualidad_UnityProject/Assets/mainMenu.cs
@@ -13,20 +13,47 @@ public class mainMenu : MonoBehaviour
     public void Iniciar()
     {
         SoundManager.Instance.PlaySound(SoundNames.ingameButton);
-        //SceneManager.LoadScene("SampleScene");
         home.SetActive(false);
         levelSelect.SetActive(true);
     }
 
+    public void Level1()
+    {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
+        SceneManager.LoadScene("SampleScene");  
+    }
+
+    public void Level2()
+    {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
+        SceneManager.LoadScene("Scene2");
+       
+    }
+
+    public void Level3()
+    {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
+        SceneManager.LoadScene("Scene3");
+    }
 
     public void Creditos()
     {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
         SceneManager.LoadScene("Credits");
     }
 
     public void Quitar()
     {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
         Application.Quit();
     }
 
+
+    public void Volver()
+    {
+        SoundManager.Instance.PlaySound(SoundNames.ingameButton);
+        SceneManager.LoadScene("MainMenu");
+        home.SetActive(true);
+        levelSelect.SetActive(false);
+    }
 }

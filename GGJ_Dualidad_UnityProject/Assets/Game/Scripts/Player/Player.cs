33532b02f64084ae5e57640f70f73a2ee23b5483
Paul Fernandez <pafb5051@gmail.com>
2022-01-29 23:56:35 -0600

    Rotate player in rigth direction

--- GGJ_Dualidad_UnityProject/Assets/Game/Scripts/Player/Player.cs
+++ GGJ_Dualidad_UnityProject/Assets/Game/Scripts/Player/Player.cs
@@ -17,13 +17,26 @@ public class Player : MonoBehaviour
     public Collider collider;
 
     Vector3 moveVector;
+    Vector3 direction;
 
     [HideInInspector] public bool active;
 
+
     public void ProcessActions(Vector2 input)
     {
+
         moveVector.x = input.x * moveSpeed * Time.deltaTime;
+        moveVector.y = 0;
         moveVector.z = input.y * moveSpeed * Time.deltaTime;
+
+        direction = Vector3.Normalize(new Vector3(input.x,0,input.y));
+
+        if (direction.magnitude != 0)
+        {
+            float angle = Mathf.Atan2(direction.z, -direction.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+        }
+        
         body.MovePosition(transform.position + moveVector);
     }
 }
